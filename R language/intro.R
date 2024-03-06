172f53542c0d4ef474891c9470dc9c8fc446ffbe
Host-Vedas <147248574+Host-Vedas@users.noreply.github.com>
2024-03-06 10:40:14 +0545

    Add files via upload

---
+++ R language/intro.R
@@ -0,0 +1,7 @@
+# it is single line comment no multiple line comment
+first_variable <- "This is variable"
+second_variable <- 12.5
+vec_1 <- c(13,17,12,45.6)
+# on othe r lattter than c
+names(x) <- c(1,12.5,"fantus")
+
